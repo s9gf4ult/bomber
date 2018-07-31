fd034b8c403bd3efbda31dd3c8642659cf515cff
Aleksey Uimanov <s9gf4ult@gmail.com>
2018-07-31 22:12:31 +0500

    Add: simple stats

--- src/Bomber.hs
+++ src/Bomber.hs
@@ -2,16 +2,20 @@ module Bomber
     ( dropBombs
     ) where
 
+import Bomber.Stat
 import Control.Applicative
 import Control.Concurrent
 import Control.Exception
+import Control.Lens
 import Control.Monad
 import Data.Foldable
+import Data.IORef
 import Data.Monoid
 import Network.Connection
 import Network.HTTP.Client
 import Network.HTTP.Client.OpenSSL
 import Network.HTTP.Client.TLS
+import Network.HTTP.Types.Status
 import OpenSSL.Session
 import Options.Applicative
 
@@ -61,16 +65,36 @@ dropBombs = do
   opts <- parseOpts
   manager <- getManager opts
   req <- parseRequest $ target opts
+  stat <- newIORef defStat
   let
-    worker = try go >>= \case
-      Left e -> case fromException e of
-        Just (_ :: AsyncException) -> return ()
-        Nothing                    -> worker
-      Right _ -> worker
-    go = void $ httpLbs req manager
+    worker = do
+      updateStat stat $ sTryingConnect +~ 1
+      try go >>= \case
+        Left e -> case fromException e of
+          Just (_ :: AsyncException) -> return ()
+          Nothing                    -> do
+            updateStat stat
+              $ (sTryingConnect -~ 1)
+              . (sExceptions +~ 1)
+            worker
+        Right resp -> do
+          let
+            code = statusCode $ responseStatus resp
+            status = if
+              | code >= 500 -> s5xx +~ 1
+              | code >= 400 -> s4xx +~ 1
+              | code >= 200 -> s2xx +~ 1
+              | otherwise   -> sxxx +~ 1
+          updateStat stat
+            $ (sTryingConnect -~ 1)
+            . status
+          worker
+    go = httpLbs req manager
   workers <- sequence $ replicate (simRequests opts) (forkIO worker)
   let
-    sleep = forever $ threadDelay 1e6
+    sleep = forever $ do
+      printStat =<< readIORef stat
+      threadDelay 1e6
     killWorkers = for_ workers $ \w -> do
       killThread w
   finally sleep killWorkers
---
+++ src/Bomber/Stat.hs
@@ -0,0 +1,27 @@
+module Bomber.Stat where
+
+import Control.Lens
+import Data.IORef
+import Data.Text.IO as T
+import Text.Shakespeare.Text
+
+data Stats = Stats
+  { _sTryingConnect :: !Int
+  , _sExceptions    :: !Int
+  , _s2xx           :: !Int
+  , _s4xx           :: !Int
+  , _s5xx           :: !Int
+  , _sxxx           :: !Int
+  }
+
+makeLenses ''Stats
+
+defStat :: Stats
+defStat = Stats 0 0 0 0 0 0
+
+printStat :: Stats -> IO ()
+printStat Stats{..} = T.putStrLn
+  [st|Connecting: #{_sTryingConnect}, Exceptions: #{_sExceptions}, 2xx: #{_s2xx}, 4xx: #{_s4xx}, 5xx: #{_s5xx}, xxx: #{_sxxx}|]
+
+updateStat :: IORef Stats -> (Stats -> Stats) -> IO ()
+updateStat ref f = atomicModifyIORef' ref ((,()) <$> f)
